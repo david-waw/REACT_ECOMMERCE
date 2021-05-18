f5fcb5c4e2b4dae459137654db54064ca782fd33
david-waw <54469809+david-waw@users.noreply.github.com>
2021-05-18 14:48:14 +0300

    fifth commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Switch, Route } from "react-router-dom"
 
 import HomePage from './page/homepage/homepage.component.jsx'
 import ShopPage from "./page/shopPage/shop.component.jsx"
+import Header from "./components/header/header.component.jsx"
 
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
