091ac5a96eaa27e5bde71db5cc14c1495369dc16
david-waw <54469809+david-waw@users.noreply.github.com>
2021-05-18 13:58:29 +0300

    fouth commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import './App.css';
 import { Switch, Route } from "react-router-dom"
 
 import HomePage from './page/homepage/homepage.component.jsx'
+import ShopPage from "./page/shopPage/shop.component.jsx"
 
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
 
     </div>
