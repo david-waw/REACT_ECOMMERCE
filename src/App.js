7e486f8bfd2a2bf8fdb57cf17dfb0c2d0ad8ddb3
david-waw <54469809+david-waw@users.noreply.github.com>
2021-05-20 10:38:08 +0300

    ninth commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 import{connect} from "react-redux"
 import HomePage from './page/homepage/homepage.component.jsx'
 import ShopPage from "./page/shopPage/shop.component.jsx"
@@ -46,7 +46,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInSignUp} />
+          <Route exact path="/signin" render={()=>this.props.currentUser ? (<Redirect to='/'/>) : (<SignInSignUp/>)} />
         </Switch>
   
       </div>
@@ -55,8 +55,10 @@ class App extends React.Component {
 
 }
 
-
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser:user=>dispatch(setCurrentUser(user))
 })
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
