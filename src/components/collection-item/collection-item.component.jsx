5e589396a326f931d7bcbdb4ec9d2b71d98010c1
david-waw <54469809+david-waw@users.noreply.github.com>
2021-05-21 09:09:09 +0300

    tenth commit

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,28 @@
 import React from "react"
+import {connect} from "react-redux"
 import "./collection-item.style.scss"
+import CustomButton from "../custom-button/custom-button.component"
+import {addItem} from "../../redux/cart/cart.actions.js"
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className="collection-item">
-        <div className="image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
-        </div>
+const CollectionItem = ({ item, addItem }) => {
+    const {name,price,imageUrl}=item
+    return(
+<div className="collection-item">
+    <div className="image"
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+    />
+    <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
     </div>
-)
-export default CollectionItem
+    <CustomButton onClick={()=>addItem(item)} inverted>ADD TO CART</CustomButton>
+</div>
+)}
+
+const mapDispatchToProps = dispatch => ({
+    addItem:item=>dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
